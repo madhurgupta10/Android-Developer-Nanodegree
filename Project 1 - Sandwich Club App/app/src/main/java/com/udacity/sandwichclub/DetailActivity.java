506742e2bc104fc8b85eaafcec3169125a75d1f7
Madhur Gupta <madhurgupta10@gmail.com>
2018-02-03 21:50:57 +0530

    Add Desc, Place of Origin, Title and Image

--- Project 1 - Sandwich Club App/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ Project 1 - Sandwich Club App/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
+
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -42,13 +46,16 @@ public class DetailActivity extends AppCompatActivity {
             closeOnError();
             return;
         }
+        //Set Title on Action Bar
+        setTitle(sandwich.getMainName());
 
-        populateUI();
+        //Set Other Views by Calling populateUI function
+        populateUI(sandwich);
+
+        //Set Image to ImageView
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
-
-        setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -56,7 +63,44 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich s) {
+        //Get Description
+        String description =  s.getDescription();
 
+        //Get Place of Origin
+        String origin = s.getPlaceOfOrigin();
+
+        //Get Also Known As
+//        List<String> alsoKnownAs = s.getAlsoKnownAs();
+//        String[] arr = alsoKnownAs.toArray(new String[alsoKnownAs.size()]);
+        String alsoKnownAsString = "";
+//        for(int i = 0; i < arr.length; i++) {
+//            alsoKnownAsString = alsoKnownAsString + arr[i] + "\n";
+//        }
+
+        //Get Ingredients
+//        List<String> ingredients = s.getIngredients();
+//        String[] arr1 = ingredients.toArray(new String[ingredients.size()]);
+        String ingredientsString = "";
+//        for(int i = 0; i < arr.length; i++) {
+//            ingredientsString = ingredientsString + arr1[i] + "\n";
+//        }
+
+
+        //Set Description to TextView
+        TextView textView = findViewById(R.id.description_tv);
+        textView.setText(description);
+
+        //Set Place of Origin to TextView
+        TextView textView1 = findViewById(R.id.origin_tv);
+        textView1.setText(origin);
+
+        //Set Also Known As to TextView
+        TextView textView2 = findViewById(R.id.also_known_tv);
+        textView2.setText(ingredientsString);
+
+        //Set Also Known As to TextView
+        TextView textView3 = findViewById(R.id.ingredients_tv);
+        textView3.setText(alsoKnownAsString);
     }
 }
--- Project 1 - Sandwich Club App/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ Project 1 - Sandwich Club App/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,68 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
+
+        Sandwich s = new Sandwich();
+
+
+        try {
+            // Get JSON Object
+            JSONObject jsonData = new JSONObject(json);
+
+            // Get Name JSON Object inside JSON Object
+            JSONObject name = jsonData.getJSONObject("name");
+
+            // Set Main Name
+            s.setMainName(name.getString("mainName"));
+
+            // Set Also Known As
+            JSONArray arrJson = name.getJSONArray("alsoKnownAs");
+            String[] arr = new String[arrJson.length()];
+            for(int i = 0; i < arrJson.length(); i++)
+                arr[i] = arrJson.getString(i);
+
+            List<String> list = Arrays.asList(arr);
+            s.setIngredients(list);
+
+            // Set Place of Origin
+            s.setPlaceOfOrigin(jsonData.getString("placeOfOrigin"));
+
+            // Set Description
+            s.setDescription(jsonData.getString("description"));
+
+            // Set Image
+            s.setImage(jsonData.getString("image"));
+
+            // Set Ingredients
+            JSONArray arrJson1 = jsonData.getJSONArray("ingredients");
+
+            Log.d("arrJson1", ""+arrJson1);
+            String[] arr1 = new String[arrJson1.length()];
+            for(int i = 0; i < arrJson1.length(); i++)
+                arr1[i] = arrJson1.getString(i);
+
+            List<String> list1 = Arrays.asList(arr1);
+            s.setIngredients(list1);
+
+            return s;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
