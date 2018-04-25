31146cc708226da009ea6927d341ea836902e84b
Madhur Gupta <madhurgupta10@gmail.com>
2018-04-25 09:55:40 +0530

    Fixed Test Case error

--- Project 5 - Build it Bigger/app/src/androidTest/java/com/udacity/gradle/builditbigger/TestCase.java
+++ Project 5 - Build it Bigger/app/src/androidTest/java/com/udacity/gradle/builditbigger/TestCase.java
@@ -1,21 +1,20 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
+import android.support.v4.util.Pair;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
 import java.util.concurrent.ExecutionException;
 
 public class TestCase extends AndroidTestCase {
 
 
-    public TestCase() {
-        String result = null;
-        try {
-            result = new EndpointsAsyncTask().execute().get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        assert result.length()!=0;
+    public void test() throws ExecutionException, InterruptedException {
+        String result = "";
+        result = new EndpointsAsyncTask().execute(new Pair<Context, String>(getContext(),
+                "Manfred")).get();
+        Log.d("Test", "test: "+result);
+        assertTrue(result.length()!=0);
     }
 }
