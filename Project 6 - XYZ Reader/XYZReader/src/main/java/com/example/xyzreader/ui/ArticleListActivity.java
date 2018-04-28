67e7dc2e11e9016e14fbccc491b78eb3f6c1ad88
Madhur Gupta <madhurgupta10@gmail.com>
2018-04-28 11:41:23 +0530

    Fixed UI

--- Project 6 - XYZ Reader/XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ Project 6 - XYZ Reader/XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -8,6 +8,8 @@ import android.content.IntentFilter;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -43,6 +45,7 @@ public class ArticleListActivity extends AppCompatActivity implements
     private Toolbar mToolbar;
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private RecyclerView mRecyclerView;
+    private CoordinatorLayout mCoordinatorLayout;
 
     private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss");
     // Use default locale format
@@ -57,7 +60,6 @@ public class ArticleListActivity extends AppCompatActivity implements
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
 
-
         final View toolbarContainerView = findViewById(R.id.toolbar_container);
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
@@ -65,12 +67,15 @@ public class ArticleListActivity extends AppCompatActivity implements
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         getLoaderManager().initLoader(0, null, this);
 
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator);
+
         if (savedInstanceState == null) {
             refresh();
         }
     }
 
     private void refresh() {
+        Snackbar.make(mCoordinatorLayout, "Articles Loaded", Snackbar.LENGTH_SHORT).show();
         startService(new Intent(this, UpdaterService.class));
     }
 
